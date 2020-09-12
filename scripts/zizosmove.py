41941082e4a024c823bb2f78fa45c1e9c1c47593
zizowasfy <70958856+zizowasfy@users.noreply.github.com>
2020-09-12 16:59:53 +0200

    Update zizosmove.py
    Enhanced User Interfacing

--- scripts/zizosmove.py
+++ scripts/zizosmove.py
@@ -25,55 +25,77 @@ def state():
     pub.publish(joints)
     # rate.sleep()
 
-    print("\n*********************\n@@@ Move the Robot to any position to start the demo ")
-
     while not rospy.is_shutdown():
 
-        usr = input("@@@ Move Robot to Position [ 0 / 1 / 2 / 3 ] ")
+        usr0= input("\n*********************\n@@@ How would you like to see the robot moving? \n (1) Assign it specified positions \n (2) pre-determined positions ")
+   
+                                    ### ASSigning Specified positions ###
 
-        if usr == 0:
-            while [round(current[0],2),round(current[1],2),round(current[2],2)] != home_pos:
+        if usr0 == 1:
+            j0 = input("\n*********************\nJoint 0 Value: ")
+            j1 = input("Joint 1 Value: ")
+            j2 = input("Joint 2 Value: ")
+            while [round(current[0],2),round(current[1],2),round(current[2],2)] != [j0,j1,j2]:
                 joints.header.stamp = rospy.Time.now()
                 joints.name = ["j0","j1","j2"]
-                joints.position = [ current[0]-((current[0]-home_pos[0])*i) , current[1]-((current[1]-home_pos[1])*i) , current[2]-((current[2]-home_pos[2])*i) ] 
+                joints.position = [ current[0]-((current[0]-j0)*i) , current[1]-((current[1]-j1)*i) , current[2]-((current[2]-j2)*i) ] 
                 current = joints.position
                 pub.publish(joints)
-                print current,home_pos
                 rate.sleep()
 
-        if usr == 1:
-            while [round(current[0],2),round(current[1],2),round(current[2],2)] != desired_pos:
-                joints.header.stamp = rospy.Time.now()
-                joints.name = ["j0","j1","j2"]
-                joints.position = [ current[0]-((current[0]-desired_pos[0])*i) , current[1]-((current[1]-desired_pos[1])*i) , current[2]-((current[2]-desired_pos[2])*i) ] 
-                current = joints.position
-                pub.publish(joints)
-                print current,desired_pos
-                rate.sleep()
+                                    ### Pre-determined positions ###
 
-        elif usr == 2:
-            while [round(current[0],2),round(current[1],2),round(current[2],2)] != desired_pos2:
-                joints.header.stamp = rospy.Time.now()
-                joints.name = ["j0","j1","j2"]
-                joints.position = [ current[0]-((current[0]-desired_pos2[0])*i) , current[1]-((current[1]-desired_pos2[1])*i) , current[2]-((current[2]-desired_pos2[2])*i) ] 
-                current = joints.position
-                pub.publish(joints)
-                print current,desired_pos2
-                rate.sleep()
+        if usr0 == 2:
+            usr = input("\n@@@ Move Robot to Position [ 0 / 1 / 2 / 3 ] ")
 
-        elif usr == 3:
-            while [round(current[0],2),round(current[1],2),round(current[2],2)] != desired_pos3:
-                joints.header.stamp = rospy.Time.now()
-                joints.name = ["j0","j1","j2"]
-                joints.position = [ current[0]-((current[0]-desired_pos3[0])*i) , current[1]-((current[1]-desired_pos3[1])*i) , current[2]-((current[2]-desired_pos3[2])*i) ] 
-                current = joints.position
-                pub.publish(joints)
-                print current,desired_pos3
-                rate.sleep()
+            if usr == 0:
+                                    ### Home POSITION ###
 
-        else:
-            print ("Invalid Position")
-    
+                while [round(current[0],2),round(current[1],2),round(current[2],2)] != home_pos:
+                    joints.header.stamp = rospy.Time.now()
+                    joints.name = ["j0","j1","j2"]
+                    joints.position = [ current[0]-((current[0]-home_pos[0])*i) , current[1]-((current[1]-home_pos[1])*i) , current[2]-((current[2]-home_pos[2])*i) ] 
+                    current = joints.position
+                    pub.publish(joints)
+                    print current,home_pos
+                    rate.sleep()
+                                    ### POSITION 1 ###
+
+            if usr == 1:
+                while [round(current[0],2),round(current[1],2),round(current[2],2)] != desired_pos:
+                    joints.header.stamp = rospy.Time.now()
+                    joints.name = ["j0","j1","j2"]
+                    joints.position = [ current[0]-((current[0]-desired_pos[0])*i) , current[1]-((current[1]-desired_pos[1])*i) , current[2]-((current[2]-desired_pos[2])*i) ] 
+                    current = joints.position
+                    pub.publish(joints)
+                    print current,desired_pos
+                    rate.sleep()
+                                    ### POSITION 2 ###
+
+            elif usr == 2:
+                while [round(current[0],2),round(current[1],2),round(current[2],2)] != desired_pos2:
+                    joints.header.stamp = rospy.Time.now()
+                    joints.name = ["j0","j1","j2"]
+                    joints.position = [ current[0]-((current[0]-desired_pos2[0])*i) , current[1]-((current[1]-desired_pos2[1])*i) , current[2]-((current[2]-desired_pos2[2])*i) ] 
+                    current = joints.position
+                    pub.publish(joints)
+                    print current,desired_pos2
+                    rate.sleep()
+
+                                    ### POSITION 3 ###
+            elif usr == 3:
+                while [round(current[0],2),round(current[1],2),round(current[2],2)] != desired_pos3:
+                    joints.header.stamp = rospy.Time.now()
+                    joints.name = ["j0","j1","j2"]
+                    joints.position = [ current[0]-((current[0]-desired_pos3[0])*i) , current[1]-((current[1]-desired_pos3[1])*i) , current[2]-((current[2]-desired_pos3[2])*i) ] 
+                    current = joints.position
+                    pub.publish(joints)
+                    print current,desired_pos3
+                    rate.sleep()
+
+            else:
+                print ("Invalid Position")
+
         
 if __name__ == '__main__':
     #state()
